27628c0fe8a68703e3df982ca96533d5e187485c
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-10-19 19:16:45 +0200

    check

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,5 +1,41 @@
-<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo 
-di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi 
+che non conosciamo nella documentazione) che name sia più lungo 
+di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+    $name = $_GET['name'];  // length maggiore di 3 caratteri
+    $mail = $_GET['mail'];  // deve contenere un punto e una chiocciola
+    $age = $_GET['age'];  // age deve essere un numero
+
+    // check name
+    if (strlen($name) > 3) {
+        echo 'name: true <br>';
+    } else {
+        echo 'name: false <br>';
+    }
+    // check age
+    if ( is_numeric($age)) {
+        echo 'age: true <br>';
+    } else {
+        echo 'age: false <br>';
+    }
+    // check mail
+    if ( filter_var($mail, FILTER_VALIDATE_EMAIL) ) {
+        echo 'mail: true <br>';
+    } else {
+        echo 'mail: false <br>';
+    }
+
+    // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+    if ( strlen($name) > 3 && is_numeric($age) && filter_var($mail, FILTER_VALIDATE_EMAIL) ) {
+        echo 'Accesso riuscito';
+    } 
+    else {
+        echo 'Accesso negato';
+    }
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +45,7 @@ di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un num
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 2</title>
 </head>
-<body>
+<body style="background-color:black; color:white; font-family: verdana; margin: 50px" >
     
 </body>
 </html>
